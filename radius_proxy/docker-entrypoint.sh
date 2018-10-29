084c53590730592b088ddc1d24e14da14881ebaf
Mohamad Haj Yahia <mhy@fb.com>
2018-10-29 03:55:37 -0700

    Add SSE_CLIENT_MAC parameter support
    Summary:
    eth0 mac address is not unique in different radius_proxy containers running on different docker environment.
    Let's take this as parameter (and randomize it if not specified).
    This will make sure no similiar sse topic on different radius_proxy instances.
    
    Reviewed By: nivnoach
    
    Differential Revision: D12818151
    
    fbshipit-source-id: e9d5c717ebbeca72f07d5ce82b4dc1dca2949715

--- radius_proxy/docker-entrypoint.sh
+++ radius_proxy/docker-entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-export ETH0_MAC_ADDR=`ifconfig | grep eth0 | awk '{print $5}'`
+if [ -z "$SSE_CLIENT_MAC" ]
+then
+	export SSE_CLIENT_MAC=$(ifconfig | grep eth0 | awk '{print $5}')
+fi
 /usr/bin/envsubst < /app/radius_proxy.config.template > /app/radius_proxy.conf
 /app/radius_proxy -c /app/radius_proxy.conf -d | tee /proc/1/fd/1 | /scribedog/scribedog -config /scribedog/scribedog.ini -file=-
